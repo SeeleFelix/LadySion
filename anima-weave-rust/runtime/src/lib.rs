c200644e9b6b86aafbaaee82633e824c1dd651f2
DI-HUO-MING-YI <DI-HUO-MING-YI@outlook.com>
2025-07-01 23:58:50 +0800

    清理掉所有的东西

--- anima-weave-rust/runtime/src/lib.rs
+++
@@ -1,4 +0,0 @@
-pub mod integration_test;
-pub mod launcher;
-
-pub use integration_test::*;
--- anima-weave-rust/vessels/src/labels/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod number_label;
-pub mod prompt_label;
-pub mod string_label;
-
-pub use number_label::*;
-pub use prompt_label::*;
-pub use string_label::*;
